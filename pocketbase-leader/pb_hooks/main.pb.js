538620479d667b3d1df4ca329323840dc78c3d5b
robmolloy <robmolloy@hotmail.co.uk>
2025-08-21 13:58:14 +0100

    create /get-providers route

--- pocketbase-leader/pb_hooks/main.pb.js
+++ pocketbase-leader/pb_hooks/main.pb.js
@@ -13,3 +13,9 @@ onRecordCreate((e) => {
 
   e.next();
 }, "users");
+
+routerAdd("GET", "/get-providers", async (e) => {
+  const resp = await $app.findCollectionByNameOrId("users");
+
+  return e.json(200, { message: resp.oauth2?.providers?.map((x) => x.name) });
+});
