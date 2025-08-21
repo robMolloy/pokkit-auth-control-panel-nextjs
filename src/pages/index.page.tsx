a79a6bf82b7d9527d453c33937cc6e711253c12e
robmolloy <robmolloy@hotmail.co.uk>
2025-08-21 18:24:02 +0100

    add simple icons - experiment

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -10,6 +10,27 @@ import {
 } from "@/modules/usersCollection/pbUsersCollectionHelpers";
 import { useEffect, useState } from "react";
 
+import { siReact, siNextdotjs, siJavascript, SimpleIcon } from "simple-icons";
+import { useThemeStore } from "@/stores/themeStore";
+
+const BrandIcon = (p: { iconData: SimpleIcon; size: number }) => {
+  const x = useThemeStore();
+  console.log(`index.page.tsx:${/*LL*/ 33}`, { x });
+  return (
+    <svg
+      role="img"
+      viewBox="0 0 24 24"
+      width={p.size}
+      height={p.size}
+      fill={`#${p.iconData.hex === "000000" ? "ffffff" : p.iconData.hex}`}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>{p.iconData.title}</title>
+      <path d={p.iconData.path} />
+    </svg>
+  );
+};
+
 export default function Home() {
   const [usersCollection, setUsersCollection] = useState<TUsersCollection>();
 
@@ -27,6 +48,9 @@ export default function Home() {
       <p className="text-muted-foreground">
         This is your dashboard. Start adding your content here.
       </p>
+      <BrandIcon iconData={siReact} size={32} />
+      <BrandIcon iconData={siNextdotjs} size={32} />
+      <BrandIcon iconData={siJavascript} size={32} />
 
       {usersCollection && (
         <>
