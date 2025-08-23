b0982402c4f2339182c3194a8b546427c84be9bf
robmolloy <robmolloy@hotmail.co.uk>
2025-08-23 16:44:21 +0100

    remove simple-icons from index page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -3,33 +3,12 @@ import { H1 } from "@/components/ui/defaultComponents";
 import { pb } from "@/config/pocketbaseConfig";
 import { OAuth2Providers } from "@/modules/usersCollection/OAuth2Providers";
 import { EnableUsersCollectionOauth2Toggle } from "@/modules/usersCollection/EnableUsersCollectionOauth2Toggle";
-
 import {
   getUsersCollection,
   TUsersCollection,
 } from "@/modules/usersCollection/pbUsersCollectionHelpers";
 import { useEffect, useState } from "react";
-
-import { siReact, siNextdotjs, siJavascript, SimpleIcon } from "simple-icons";
-import { useThemeStore } from "@/stores/themeStore";
-
-const BrandIcon = (p: { iconData: SimpleIcon; size: number }) => {
-  const x = useThemeStore();
-  console.log(`index.page.tsx:${/*LL*/ 33}`, { x });
-  return (
-    <svg
-      role="img"
-      viewBox="0 0 24 24"
-      width={p.size}
-      height={p.size}
-      fill={`#${p.iconData.hex === "000000" ? "ffffff" : p.iconData.hex}`}
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <title>{p.iconData.title}</title>
-      <path d={p.iconData.path} />
-    </svg>
-  );
-};
+import { OAuth2ProviderImage, providerNames } from "@/modules/usersCollection/OAuth2ProviderImage";
 
 export default function Home() {
   const [usersCollection, setUsersCollection] = useState<TUsersCollection>();
@@ -48,10 +27,6 @@ export default function Home() {
       <p className="text-muted-foreground">
         This is your dashboard. Start adding your content here.
       </p>
-      <BrandIcon iconData={siReact} size={32} />
-      <BrandIcon iconData={siNextdotjs} size={32} />
-      <BrandIcon iconData={siJavascript} size={32} />
-
       {usersCollection && (
         <>
           <EnableUsersCollectionOauth2Toggle
