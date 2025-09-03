b8b54c15ff094e40bc4cb3be2a0d10182b1717dc
robmolloy <robmolloy@hotmail.co.uk>
2025-09-03 07:26:32 +0100

    add AccordionCard

---
+++ src/components/AccordionCard.tsx
@@ -0,0 +1,30 @@
+import { Accordion, AccordionContent, AccordionTrigger } from "@/components/ui/accordion";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { AccordionItem } from "@radix-ui/react-accordion";
+
+export const AccordionCard = (p: {
+  value: string;
+  topLeft?: React.ReactNode;
+  title?: React.ReactNode;
+  subtitle?: React.ReactNode;
+  content?: React.ReactNode;
+}) => {
+  return (
+    <Card className="p-0">
+      <CardContent className="p-0">
+        <Accordion type="single" collapsible className="w-full px-6">
+          <AccordionItem value={p.value}>
+            <AccordionTrigger className="flex justify-start gap-4">
+              {p.topLeft}
+              <div className="flex flex-1 flex-col gap-2">
+                <CardTitle className="flex gap-4">{p.title}</CardTitle>
+                <div className="no-underline">{p.content}</div>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent className="p-0">{p.content}</AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </CardContent>
+    </Card>
+  );
+};
