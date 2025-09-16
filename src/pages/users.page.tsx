41bcc3ef942b7708abf62c484934f48140f4babe
robmolloy <robmolloy@hotmail.co.uk>
2025-09-16 19:53:20 +0100

    users page, no pagination

--- src/pages/users.page.tsx
+++ src/pages/users.page.tsx
@@ -1,5 +1,4 @@
 import { MainLayout } from "@/components/layout/LayoutTemplate";
-import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -9,7 +8,6 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { pb } from "@/config/pocketbaseConfig";
 import { useUsersStore } from "@/modules/users/usersStore";
 
 export const UsersScreen = () => {
@@ -17,29 +15,26 @@ export const UsersScreen = () => {
 
   return (
     <MainLayout>
-      <Button onClick={() => pb.collection("_superusers").authRefresh()}>Auth refresh</Button>
-
-      <pre>{JSON.stringify(usersStore.data, undefined, 2)}</pre>
-
       <br />
       <br />
 
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
+            <TableHead>Id</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Email</TableHead>
           </TableRow>
         </TableHeader>
+
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
-          </TableRow>
+          {usersStore.data.map((x) => (
+            <TableRow key={x.id}>
+              <TableCell className="font-medium">{x.id}</TableCell>
+              <TableCell className="font-medium">{x.name}</TableCell>
+              <TableCell className="font-medium">{x.email}</TableCell>
+            </TableRow>
+          ))}
         </TableBody>
         <TableCaption>A list of your recent invoices.</TableCaption>
       </Table>
